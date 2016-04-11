4ecba5b8364b4a1f21286fb2cb7595bfd133246a
Hdp Tech <noreply@hdp-tech.ru>
2016-04-12 00:42:28 +0300

    Hdp json parse

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-var express = require('express')
-var app = express()
- 
-app.get('/start', function (req, res) {
-    // echo hdp.json meta data
-    res.send('meta stuff');
-});
+var express = require('express');
+var app = express();
+var jsonfile = require('jsonfile');
+var util = require('util');
 
-app.get('/invoke', function (req, res) {
-    // invoke a function with params
-    res.send('invoke');
-});
+var file = './config/hdp.json.example';
+var hdpJson = '';
+jsonfile.readFile(file, function(err, obj) {
+    if (err) throw err;
+    hdpJson = obj;
+    
+    app.get('/start', function (req, res) {
+        res.send(hdpJson);
+    });
 
-app.get('*', function(req, res) {
-    res.redirect('/start');
+    app.get('/invoke', function (req, res) {
+        // invoke a function with params
+        res.send('invoke');
+    });
+
+    app.get('*', function(req, res) {
+        res.redirect('/start');
+    });
 });
  
 app.listen(3000, '127.0.0.1');
