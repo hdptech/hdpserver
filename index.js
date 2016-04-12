973599b7cccaca226beed2afd7ea67e864298582
Hdp Tech <hdp.tech@yandex.ru>
2016-04-13 00:08:15 +0300

    Async waterfall for index file

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ var express = require('express');
 var app = express();
 var jsonfile = require('jsonfile');
 var util = require('util');
+var async = require('async');
 
 var file = './config/hdp.json.example';
-var hdpJson = '';
-jsonfile.readFile(file, function(err, obj) {
+
+async.waterfall([
+    getHdpJson,
+    registerUrlHandlers
+], function (err, result) {
     if (err) throw err;
-    hdpJson = obj;
-    
+    console.log(result);
+});
+function getHdpJson(callback) {
+    jsonfile.readFile(file, function(err, obj) {
+        if (err) return callback(err, null);
+        callback(null, obj);
+    });
+}
+function registerUrlHandlers(hdpJson, callback) {
     app.get('/start', function (req, res) {
         res.send(hdpJson);
     });
@@ -21,6 +32,8 @@ jsonfile.readFile(file, function(err, obj) {
     app.get('*', function(req, res) {
         res.redirect('/start');
     });
-});
- 
-app.listen(3000, '127.0.0.1');
+    
+    app.listen(3000, '127.0.0.1');
+    
+    callback(null, 'registered');
+}
