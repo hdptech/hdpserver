e9a9db0a62679475b7f7e37a7a1809a86944a568
Hdp Tech <hdp.tech@yandex.ru>
2016-04-21 23:54:07 +0300

    Forward requests

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 var jsonfile = require('jsonfile');
 var util = require('util');
 var async = require('async');
@@ -24,11 +29,29 @@ function registerUrlHandlers(hdpJson, callback) {
         res.send(hdpJson);
     });
 
-    app.get('/invoke', function (req, res) {
-        // invoke a function with params
+    app.post('/invoke', urlencodedParser, function (req, res) {
+        var requestedFunction = req.body.function;
+        var functionStuff;
+        
+        for (var i = 0; i < hdpJson['functions'].length; i++) {
+            if (requestedFunction == hdpJson['functions'][i]['name']) {
+                functionStuff = hdpJson['functions'][i];
+                break;
+            }
+        }
+        
+        if (requestedFunction !== undefined) {            
+            request.get({url:functionStuff.upstream, form: {"test" : 123}},
+            function(err,httpResponse,body){
+            })
+            
+        } else {
+            console.log('No function available');
+        }
+        
         res.send('invoke');
     });
-
+    
     app.get('*', function(req, res) {
         res.redirect('/start');
     });
